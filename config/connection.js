9f151db1b73e63fe60525feaf4769882eac4b07f
KLIV <wclivingston5@gmail.com>
2018-09-26 18:50:17 -0500

    update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,38 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "121025",
-  database: "menu_db"
-});
+let config;
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+if(process.env.JAWSDB_URL) {
+  config = process.env.JAWSDB_URL;
+} else {
+  config = {
+    host: "localhost",
+    user: "root",
+    password: "121025",
+    database: "menu_db"
+  };
+}
+
+module.exports = mysql.createConnection(config);
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "121025",
+//   database: "menu_db"
+// });
+
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
 
 // Export connection for our ORM to use.
-module.exports = connection;
+// module.exports = connection;
